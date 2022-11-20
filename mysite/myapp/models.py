937343414ea5c227482da47a20ebe0de2f1a2a30
Souhail El Hayani <113797837+souhailelhayani@users.noreply.github.com>
2022-11-19 23:15:43 -0500

    Souhail changes (#2)
    * Update models.py
    
    * Delete db.sqlite3
    
    * Add files via upload
    
    * Update admin.py
    
    * Add files via upload
    
    * Update urls.py
    
    * Update views.py
    
    * Add files via upload
    
    * Rename index.html to homepage.html
    
    * Update views.py
    
    * Update MainMenu.html

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,5 +1,29 @@
 from django.db import models
 
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+# Create your models here.
+
+class Course(models.Model):
+    id=models.CharField(max_length=4, primary_key=True)
+    name=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class User(models.Model):
+    id=models.CharField(max_length=9, primary_key=True)
+    birth_year=models.IntegerField(default=0)
+    nationality=models.CharField(max_length=20, default="")
+    fullname=models.CharField(max_length=20)
+    password=models.CharField(max_length=16,default="")
+
+    def __str__(self):
+        return self.fullname
+
+class Course_taker(models.Model):
+    """course is named course_id in table"""
+    course=models.ForeignKey(Course, on_delete=models.CASCADE)
+    """user is named user_id in table"""
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.course.id
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -55,3 +55,71 @@ def mainmenu(request):
     except:
         return Http404
 
+    
+def homepage(request):
+    return render(request, "myapp/homepage.html")
+
+def users(request):
+    #all lists should be in order, each index in every list will be for one user
+    """user=[]
+    '''names=[]
+    ids=[]
+    nationalities=[]
+    years=[]'''
+    cursor=conn.execute("SELECT id,birth_year,nationality,fullname FROM myapp_user")
+    for row in cursor:
+        '''names.append(row[3])
+        ids.append(row[0])
+        nationalities.append(row[1])
+        years.append(row[2])'''
+        user.append(row)
+
+    conn.close()"""
+
+    user=User.objects.all()
+    context={"users_list":user}
+
+    return render(request, "myapp/users.html", context)
+
+def courses(request):
+    course=Course.objects.all()
+    context={"courses_list":course}
+    return render(request, "myapp/courses.html",context)
+
+def course_takers(request):
+    course_taker=Course_taker.objects.all()
+    context={"course_takers_list":course_taker}
+    return render(request,"myapp/course_takers.html",context)
+
+def newUser(request):
+    if request.method=='POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/homepage")
+    else:
+        form = UserForm()
+    return render(request,"myapp/newUser.html",{"userform":form})
+
+def newCourse(request):
+    if request.method=='POST':
+        form = CourseForm(request.POST) 
+        if form.is_valid():
+            form.save()
+            return redirect("/homepage")
+        
+    else:
+        form = CourseForm()
+    return render(request,"myapp/newCourse.html",{"courseform":form})
+
+def newCourse_taker(request):
+    if request.method=='POST':
+        form = Course_takerForm(request.POST) 
+        if form.is_valid():
+            form.save()
+            return redirect("/homepage")
+        
+    else:
+        form = Course_takerForm()
+    return render(request,"myapp/newCourse_taker.html",{"course_takerform":form})
+
