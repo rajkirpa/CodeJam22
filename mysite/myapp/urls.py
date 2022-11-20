87d37a8009db9df511c585f36bb83df5fdaf4be7
Paul Kevorkov <55221332+paulkevorkov@users.noreply.github.com>
2022-11-19 22:30:59 -0500

    Update urls.py

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('signup', views.signup, name='signup'),
-    path('signin', views.signin, name='signin'),
-    path('signout', views.signout, name='signout')
+    path("", views.home, name = "home"),
+    path("signup", views.signup, name = "signup"),
+    path("mainmenu", views.mainmenu, name="main-menu")
 ]
